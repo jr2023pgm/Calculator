dee33f4b0c7d91656fa1b3b6787b3ed5e4bf148b
Jay Ram <jr2023pgm@gmail.com>
2023-06-28 20:52:18 +0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by jr on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalculatorView()
+        }
+    }
+}
---
+++ Calculator/CalculatorView.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//  Created by jr on 28/06/2023.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("0").foregroundColor(.white)
+            // TODO: ButtonPad
+        }
+        .background(Color.black)
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
